07bd9d2571cfd718d44078ff2be6259676092fc9
Dmitry <gamexgroup@gmail.com>
2018-12-16 23:35:06 +0700

    add ReadOne, ReadAll handlers

--- cat-rating-backend/routes/cat.js
+++ cat-rating-backend/routes/cat.js
@@ -1,19 +1,40 @@
 
+var ObjectID = require('mongodb').ObjectID;
 
 module.exports = (app, db) => {
-  app.post('/cats', (req, res) => {
+
+  app.get('/cats/:id', async (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    try {
+      const cat = await db.collection('cats').findOne(details);
+      res.send(cat);
+    } catch (err) {
+      res.send({'error':'An error has occurred'});
+    }
+  });
+
+  app.get('/cats', async (req, res) => {
+    try {
+      const cats = await db.collection('cats').find({}).toArray();
+      res.send(cats);
+    } catch (err) {
+      res.send({'error':'An error has occurred'});
+    }
+  });
+
+  app.post('/cats', async (req, res) => {
     const cat = {
       //id: req.body.body,
       name: req.body.name,
       icon: req.body.icon,
       like: req.body.like,
     };
-    db.collection('cats').insert(cat, (err, result) => {
-      if (err) {
-        res.send({ 'error': 'An error has occurred' });
-      } else {
-        res.send(result.ops[0]);
-      }
-    });
-  });
+    try {
+      const result = await db.collection('cats').insert(cat);
+      res.send(result.ops[0]);
+    } catch (err) {
+      res.send({ 'error': 'An error has occurred' });
+    }
+  }); // End post cats
 };

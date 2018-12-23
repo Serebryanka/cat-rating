707448a8177382ef6c15a8ea92ff816b767a35fd
Dmitry <gamexgroup@gmail.com>
2018-12-23 15:08:05 +0700

    add store

--- cat-rating-web/src/App.js
+++ cat-rating-web/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import Root from './Root';
+
+import rootReducer from "./reducers";
+
+const store = createStore(rootReducer);
+
 
 const theme = createMuiTheme();
 
 const App = () => (
-  <MuiThemeProvider theme={theme}>
-    <Root />
-  </MuiThemeProvider>
+  <Provider store={store}>
+    <MuiThemeProvider theme={theme}>
+      <Root />
+    </MuiThemeProvider>
+  </Provider>
 );
 
 export default App;

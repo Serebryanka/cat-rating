e99ccf99c4d1c94e42a6c1265519769565e9eac5
Dmitry <gamexgroup@gmail.com>
2018-12-23 15:07:14 +0700

    add reducer cats

---
+++ cat-rating-web/src/reducers/cats.js
@@ -0,0 +1,51 @@
+import { ADD_CAT, REMOVE_CAT, TOGGLE_LIKE } from "../actions/cats";
+
+const initialState = {
+  items: [
+    {
+      id: 1,
+      name: "Kisa",
+      icon: "https://cmkt-image-prd.global.ssl.fastly.net/0.1.0/ps/1770700/1160/772/m1/fpnw/wm0/black-cat-icon-flat-01-.jpg?1476726021&s=c956a64f60cd25e39dcd353c52e188a1",
+      like: true,
+    },
+    {
+      id: 2,
+      name: "Kotya",
+      icon: "https://cdn3.iconfinder.com/data/icons/cat-force/256/cat_hungry.png",
+      like: true,
+    },
+  ],
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case ADD_CAT: {
+      const { item } = action.payload;
+      return {
+        ...state,
+        items: [...state.items, item],
+      };
+    }
+    case REMOVE_CAT: {
+      const {id} = action.payload;
+      return {
+        ...state,
+        items: state.items.filter(item => item.id !== id),
+      };
+    }
+    case TOGGLE_LIKE: {
+      const {id} = action.payload;
+      return {
+        ...state,
+        items: state.items.map(item => {
+  				return item.id === id ? {
+  						...item,
+  						like: !item.like,
+  					} : item;
+          }),
+      };
+    }
+    default:
+      return state;
+  }
+}

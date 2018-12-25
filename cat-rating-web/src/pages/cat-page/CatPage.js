5a0c07e8cf9c932a9d7753f76c90add6c86b68f8
Dmitry <gamexgroup@gmail.com>
2018-12-25 23:03:26 +0700

    use containers instead of pure components

--- cat-rating-web/src/pages/cat-page/CatPage.js
+++ cat-rating-web/src/pages/cat-page/CatPage.js
@@ -2,50 +2,18 @@ import React, { Component } from 'react';
 import logo from '../../logo.svg';
 import '../../App.css';
 import CatList from '../../containers/cat-list';
-import CatSearch from '../../components/cat-search';
-import CatSort from '../../components/cat-sort';
+import CatSearch from '../../containers/cat-search';
+import CatSort from '../../containers/cat-sort';
 import CatAdd from '../../containers/cat-add';
 
 
 class CatPage extends Component {
 	state = {
 		text: "Pelmeshki",
-		filteredName: '',
-		sortField: '',
 	}
-	handleFilteredNameChange = name => {
-		this.setState({
-      filteredName: name,
-    });
-  };
-	handleSortChange = sortBy => {
-		this.setState({
-			sortField: sortBy,
-		})
-	}
-  render() {
-		const {text, filteredName, sortField} = this.state;
-		/*const sortedCats = cats;
-		sortedCats.sort((catA, catB) => {
-			if (sortField === 'Name') {
-				if (catA.name.toLowerCase() < catB.name.toLowerCase())
-			    return -1;
-			  if (catA.name.toLowerCase() > catB.name.toLowerCase())
-			    return 1;
-			  return 0;
-			} else if (sortField === 'Like') {
-				if (catA.like < catB.like)
-			    return -1;
-			  if (catA.like > catB.like)
-			    return 1;
-			  return 0;
-			}
-			return 0;
-		});
 
-		cats={(sortField === '' ? sortedCats : cats)
-		  .filter(cat => cat.name.toLowerCase().indexOf(filteredName.toLowerCase()) !== -1)}
-		*/
+  render() {
+		const {text} = this.state;
 		return (
       <div className="App">
         <header className="App-header">
@@ -64,9 +32,7 @@ class CatPage extends Component {
 					<CatSort
 						onSortChange={this.handleSortChange}
 					/>
-					<CatSearch
-						onSearchChanged={this.handleFilteredNameChange}
-					/>
+					<CatSearch />
 					<div>
 						<CatList />
 		      </div>

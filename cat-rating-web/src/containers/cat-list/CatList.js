e3118a9bc98db04e9a0781cffcb420ab98d0feaa
Dmitry <gamexgroup@gmail.com>
2018-12-25 23:02:46 +0700

    use selector FilteredAndSortedCats

--- cat-rating-web/src/containers/cat-list/CatList.js
+++ cat-rating-web/src/containers/cat-list/CatList.js
@@ -1,16 +1,19 @@
 import { connect } from "react-redux";
 import { removeCat, toggleLike } from "../../actions/cats";
+import { getFilteredAndSortedCats } from "../../selectors/cats";
 
 import CatList from '../../components/cat-list';
 
-const mapStateToProps = state => ({
-  cats: state.cats.items,
-});
+const mapStateToProps = state => {
+  const { searchFilter, sorting } = state;
+  const cats = getFilteredAndSortedCats(state, sorting.sortField, searchFilter.value);
+  return { cats };
+};
 
 export default connect(
   mapStateToProps,
   {
     onDelete: removeCat,
     onLikeChanged: toggleLike,
-}
+  }
 )(CatList);

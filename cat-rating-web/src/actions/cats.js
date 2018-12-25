dd832aa5530dd5dd652b99f167fb89872a9edcf9
Dmitry <gamexgroup@gmail.com>
2018-12-25 23:00:35 +0700

    add actions Set_Filter, Set_Sorting

--- cat-rating-web/src/actions/cats.js
+++ cat-rating-web/src/actions/cats.js
@@ -1,6 +1,9 @@
 export const ADD_CAT = "cats/ADD_CAT";
 export const REMOVE_CAT = "cats/REMOVE_CAT";
 export const TOGGLE_LIKE = "cats/TOGGLE_LIKE";
+export const SET_FILTER = "cats/SET_FILTER";
+export const SET_SORTING = "cats/SET_SORTING";
+
 
 let nextCatId = 2;
 
@@ -25,5 +28,19 @@ export const toggleLike = id => ({
   type: TOGGLE_LIKE,
   payload: {
     id: id,
+  },
+});
+
+export const setFilter = text => ({
+  type: SET_FILTER,
+  payload: {
+    value: text,
+  },
+});
+
+export const setSorting = sortBy => ({
+  type: SET_SORTING,
+  payload: {
+    sortField: sortBy,
   },
 });

9cc59ace94742a46b4c8b237ef1df149bf0544a5
Dmitry <gamexgroup@gmail.com>
2019-01-01 17:51:32 +0700

    use cors package

--- cat-rating-backend/server.js
+++ cat-rating-backend/server.js
@@ -2,10 +2,12 @@ const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const db             = require('./config/db');
+const cors           = require('cors');
 const app            = express();
 
 const port = 8000;
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 MongoClient.connect(db.url, (err, client) => {

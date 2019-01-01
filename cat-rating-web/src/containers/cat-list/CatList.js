4072dc18ad624b3178c1038e7069be72609358df
Dmitry <gamexgroup@gmail.com>
2019-01-01 17:56:41 +0700

    add Container component with onRequest

--- cat-rating-web/src/containers/cat-list/CatList.js
+++ cat-rating-web/src/containers/cat-list/CatList.js
@@ -1,8 +1,41 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from "react-redux";
-import { removeCat, toggleLike } from "../../actions/cats";
+import { removeCat, toggleLike, fetchCats } from "../../actions/cats";
 import { getFilteredAndSortedCats } from "../../selectors/cats";
 
 import CatList from '../../components/cat-list';
+
+
+class Container extends Component {
+  // Overrides.
+  componentWillMount() {
+    this.props.onRequest()
+  }
+  render() {
+    const {cats, onDelete, onLikeChanged} = this.props;
+    return (
+      <CatList
+        cats={cats}
+        onDelete={onDelete}
+        onLikeChanged={onLikeChanged}
+      />
+    );
+  }
+}
+
+
+Container.propTypes = {
+  cats: PropTypes.arrayOf(PropTypes.shape({
+		id: PropTypes.string.isRequired,
+		name: PropTypes.string.isRequired,
+		icon: PropTypes.string.isRequired,
+		like: PropTypes.bool.isRequired,
+	})).isRequired,
+	onDelete: PropTypes.func.isRequired,
+	onLikeChanged: PropTypes.func.isRequired,
+  onRequest: PropTypes.func.isRequired,
+}
 
 const mapStateToProps = state => {
   const { searchFilter, sorting } = state;
@@ -12,8 +45,11 @@ const mapStateToProps = state => {
 
 export default connect(
   mapStateToProps,
-  {
+  dispatch => ({
+  	onRequest: () => {
+  		dispatch(fetchCats())
+  	},
     onDelete: removeCat,
     onLikeChanged: toggleLike,
-  }
-)(CatList);
+  })
+)(Container);

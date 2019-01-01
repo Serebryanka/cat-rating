6f44d6113c3812d12b56c674b4752b5046664000
Dmitry <gamexgroup@gmail.com>
2019-01-01 17:54:25 +0700

    add fetch action

--- cat-rating-web/src/actions/cats.js
+++ cat-rating-web/src/actions/cats.js
@@ -3,9 +3,27 @@ export const REMOVE_CAT = "cats/REMOVE_CAT";
 export const TOGGLE_LIKE = "cats/TOGGLE_LIKE";
 export const SET_FILTER = "cats/SET_FILTER";
 export const SET_SORTING = "cats/SET_SORTING";
+export const FETCH_CATS = "cats/FETCH_CATS";
 
 
 let nextCatId = 2;
+
+export const fetchCats = () => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch('http://localhost:8000/cats');
+      const cats = await response.json();
+      dispatch({
+        type: FETCH_CATS,
+        payload: {
+          items: cats,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
 
 export const addCat = item => ({
   type: ADD_CAT,

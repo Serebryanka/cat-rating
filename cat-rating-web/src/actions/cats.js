347588637d74eaf0eb3c71efa4d5ae04bd70400c
Dmitry <gamexgroup@gmail.com>
2019-01-02 15:32:21 +0700

    use backend in actions removeCat and setLike (toggleLike action was changed to setLike)

--- cat-rating-web/src/actions/cats.js
+++ cat-rating-web/src/actions/cats.js
@@ -1,19 +1,17 @@
 export const ADD_CAT = "cats/ADD_CAT";
 export const REMOVE_CAT = "cats/REMOVE_CAT";
-export const TOGGLE_LIKE = "cats/TOGGLE_LIKE";
+export const SET_LIKE = "cats/SET_LIKE";
 export const SET_FILTER = "cats/SET_FILTER";
 export const SET_SORTING = "cats/SET_SORTING";
 export const FETCH_CATS = "cats/FETCH_CATS";
 
 
-let nextCatId = 2;
 
 export const fetchCats = () => {
   return async (dispatch) => {
     try {
       const response = await fetch('http://localhost:8000/cats');
       const cats = await response.json();
-      console.log(cats);
       dispatch({
         type: FETCH_CATS,
         payload: {
@@ -49,29 +47,44 @@ export const addCat = item => {
   };
 };
 
-/*export const addCat = item => ({
-  type: ADD_CAT,
-  payload: {
-    item: {
-      ...item,
-      id: ++nextCatId,
+export const removeCat = id => {
+  return async (dispatch) => {
+    try {
+      const address = `http://localhost:8000/cats/${id}`;
+      const response = await fetch(address, {
+        method: 'DELETE',
+      });
+      const result = await response.json();
+      dispatch({
+        type: REMOVE_CAT,
+        payload: {
+          id: result.success ? id : '',
+        },
+      });
+    } catch (err) {
+      console.log(err);
     }
-  },
-});*/
+  };
+};
 
-export const removeCat = id => ({
-  type: REMOVE_CAT,
-  payload: {
-    id: id,
-  },
-});
-
-export const toggleLike = id => ({
-  type: TOGGLE_LIKE,
-  payload: {
-    id: id,
-  },
-});
+export const setLike = (id, value) => {
+  return async (dispatch) => {
+    try {
+      const address = `http://localhost:8000/cats/${id}/like?value=${value}`;
+      const response = await fetch(address);
+      const result = await response.json();
+      dispatch({
+        type: SET_LIKE,
+        payload: {
+          id: result.success ? id : '',
+          like: value,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
 
 export const setFilter = text => ({
   type: SET_FILTER,

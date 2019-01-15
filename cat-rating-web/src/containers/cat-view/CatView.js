da5551387e3aeef31cfe2d3ce0482dbca000fa4e
Dmitry <gamexgroup@gmail.com>
2019-01-15 23:59:15 +0700

    attach CatView to cat reducer

--- cat-rating-web/src/containers/cat-view/CatView.js
+++ cat-rating-web/src/containers/cat-view/CatView.js
@@ -1,16 +1,55 @@
-
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from "react-redux";
+import { fetchCatByID } from "../../actions/cat";
 
 import CatView from '../../components/cat-view';
 
+class CatContainer extends Component {
+  // Overrides.
+  componentWillMount() {
+    const { onRequest, catID } = this.props;
+    onRequest(catID);
+  }
+  render() {
+    const { cat, loading, loadErr } = this.props;
+    return (
+      <CatView
+        cat={cat}
+        loading={loading}
+        loadErr={loadErr}
+      />
+    );
+  }
+}
+
+CatContainer.propTypes = {
+  cat: PropTypes.shape({
+		id: PropTypes.string.isRequired,
+		name: PropTypes.string.isRequired,
+		icon: PropTypes.string.isRequired,
+		like: PropTypes.bool.isRequired,
+	}).isRequired,
+  loading: PropTypes.bool.isRequired,
+  loadErr: PropTypes.any.isRequired,
+	onDelete: PropTypes.func.isRequired,
+  onRequest: PropTypes.func.isRequired,
+}
+
 const mapStateToProps = (state, ownProps) => {
-  const cat = state.cats.items.find(item => item.id === ownProps.catID);
   return {
-    cat,
+    cat: state.cat.data,
+    loading: state.cat.fetching,
+    loadErr: state.cat.fetchErr,
   };
 };
 
+
 export default connect(
   mapStateToProps,
-  null
-)(CatView);
+  dispatch => ({
+  	onRequest: (catID) => {
+  		dispatch(fetchCatByID(catID))
+  	},
+  })
+)(CatContainer);

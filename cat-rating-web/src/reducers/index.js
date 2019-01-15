85efb2fd3fb2ea28b64c993ba13de1920b30d0cd
Dmitry <gamexgroup@gmail.com>
2019-01-15 23:58:33 +0700

    use cat reducer

--- cat-rating-web/src/reducers/index.js
+++ cat-rating-web/src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
 //import visibilityFilter from "./visibilityFilter";
+import cat from "./cat";
 import cats from "./cats";
 import comments from "./comments";
 import searchFilter from "./searchFilter";
 import sorting from "./sorting";
 
 export default combineReducers({
+  cat,
   cats,
   comments,
   searchFilter,
